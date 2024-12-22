ebbf84b66c77d0200716b216a952dae24d039761
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-22 19:20:12 -0300

    feat: add router implementation using Elysia for dynamic route loading

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import Elysia from 'elysia';
+
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+export const router = async (dirname: string) => {
+  const app = new Elysia({ prefix: '' });
+
+  const directory = join(dirname, 'routes');
+
+  for (const file of readdirSync(directory))
+    app.use((await import(join(directory, file))).default);
+
+  return app;
+};

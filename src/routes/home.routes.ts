d27d612d58b6dbadd2a02690e7b47617e7395637
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-22 20:58:27 -0300

    fix: update import path for Home component and enhance stream rendering with bootstrap scripts

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -4,12 +4,12 @@ import { createElement } from 'react'
 import { StaticRouter } from 'react-router-dom'
 import { renderToReadableStream } from 'react-dom/server'
 
-import Home from '../react/Home'
+import Home from '@/react/Home'
 
-const router = new Elysia({ tags: ['Home'] }).get('/', async ({ request }) => {
-  const app = createElement(StaticRouter, { location: request.url }, createElement(Home))
+const router = new Elysia({ tags: ['Home'] }).get('/', async ({ request: { url } }) => {
+  const app = createElement(StaticRouter, { location: url }, createElement(Home))
 
-  const stream = await renderToReadableStream(app)
+  const stream = await renderToReadableStream(app, { bootstrapScripts: ['/public/index.js'] })
 
   return new Response(stream, {
     headers: { 'Content-Type': 'text/html' }

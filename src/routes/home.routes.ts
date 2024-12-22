dfd17fe2693904ce84ba63126edbc20b9bc608d8
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-22 20:51:11 -0300

    refactor: remove App component and update routing to use Home directly

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -4,10 +4,10 @@ import { createElement } from 'react'
 import { StaticRouter } from 'react-router-dom'
 import { renderToReadableStream } from 'react-dom/server'
 
-import App from '../react/App'
+import Home from '../react/Home'
 
 const router = new Elysia({ tags: ['Home'] }).get('/', async ({ request }) => {
-  const app = createElement(StaticRouter, { location: request.url }, createElement(App))
+  const app = createElement(StaticRouter, { location: request.url }, createElement(Home))
 
   const stream = await renderToReadableStream(app, {
     bootstrapScripts: ['/public/index.js']

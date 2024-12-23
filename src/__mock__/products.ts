a393c4c0a59784835bd3b5f4b882f82be57a01fe
DIEGOHORVATTI <d.horvattid@gmail.com>
2024-12-23 03:09:59 -0300

    feat: add mock products data for testing and development

---
+++ src/__mock__/products.ts
@@ -0,0 +1,14 @@
+export const mockProducts = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 99.99,
+    description: 'A great product'
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 149.99,
+    description: 'Another amazing product'
+  }
+]
